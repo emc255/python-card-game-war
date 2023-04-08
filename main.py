4d9a0904adaf367befa0cf693198a5fb9d7f3136
michael <micha3l_255@yahoo.com>
2023-04-08 17:36:48 -0500

    refactor

--- main.py
+++ main.py
@@ -22,14 +22,17 @@ def flip_card(player_one: Player, player_two: Player):
         player_one_in_game_cards.append(player_one_drawn_card)
         player_two_in_game_cards.append(player_two_drawn_card)
 
+        print(f"{player_one_drawn_card} vs  {player_two_drawn_card}")
         if player_one_drawn_card.rank.value > player_two_drawn_card.rank.value:
             player_one_in_game_cards.extend(player_two_in_game_cards)
             player_one.add_collected_cards(player_one_in_game_cards)
+            print(f"Player {player_one.name} wins and takes {len(player_one_in_game_cards)} cards")
             break
 
         if player_two_drawn_card.rank.value > player_one_drawn_card.rank.value:
             player_one_in_game_cards.extend(player_two_in_game_cards)
             player_two.add_collected_cards(player_one_in_game_cards)
+            print(f"Player {player_two.name} wins and takes {len(player_one_in_game_cards)} cards")
             break
 
         if player_one_drawn_card.rank.value == player_two_drawn_card.rank.value:
@@ -49,6 +52,7 @@ def flip_card(player_one: Player, player_two: Player):
                 player_one.add_collected_cards(player_one_in_game_cards)
                 break
             else:
+                print("WAR GAME!!!!")
                 player_one_in_game_cards.extend(player_one.remove_many_playing_cards(3))
                 player_two_in_game_cards.extend(player_two.remove_many_playing_cards(3))
 
@@ -61,21 +65,26 @@ def start():
     deck = Deck()
     player_one = Player("jessica")
     player_two = Player("olivia")
-    game_over = False
 
     deck.import_shuffle()
     deal(deck, player_one, player_two)
-    while not game_over:
+
+    round_number = 1
+    while True:
         flip_card(player_one, player_two)
-        if len(player_one) == 0 or len(player_two) == 0:
-            game_over = True
-            if len(player_one) > 0:
-                print("player one wins")
-            else:
-                print("player two wins")
-        elif len(player_one.playing_cards) == 0 or len(player_two.playing_cards) == 0:
+
+        if len(player_one) == 0:
+            print(f"Round {round_number}: Player {player_two.name} wins")
+            break
+        if len(player_two) == 0:
+            print(f"Round {round_number}: Player {player_one.name} wins")
+            break
+
+        if len(player_one.playing_cards) == 0 or len(player_two.playing_cards) == 0:
             player_one.shuffle()
             player_two.shuffle()
+
+        round_number += 1
 
 
 start()
